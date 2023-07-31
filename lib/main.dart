50e403bdfd873dcaa2fe87ea0e01ba87eecb430a
Raoof-ALamery <132309102+Raoof-ALamery@users.noreply.github.com>
2023-07-31 03:29:25 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'View/BottomNavigationBar/navBar.dart';
+import 'View/Home/homePage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: NavBar(),
+      theme: ThemeData(fontFamily: 'Questrial'),
+    );
+  }
+}
+
+List my_list = ["adventures", "caves", "nature", "oceans"];
